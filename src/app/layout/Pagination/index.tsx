8f0cfe23a340e896bd8220dd494d52a75cceeb29
Scatenarafael <rafascatena@gmail.com>
2021-08-09 23:40:45 -0300

    inicio do desafio - Paginação realizada com sucesso

---
+++ src/app/layout/Pagination/index.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Pagination } from "react-bootstrap";
+
+interface PaginationProps {
+  totalCountOfRegisters?: number;
+  registerPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export function PaginationComponent({
+  totalCountOfRegisters=500,
+  registerPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.ceil(totalCountOfRegisters / registerPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - (siblingsCount + 1), currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
+  return (
+    <div>
+      <Pagination>
+        {currentPage > 1 + siblingsCount && (
+          <>
+            <Pagination.Item
+              onClick={() => {
+                onPageChange(1);
+              }}
+            >
+              {1}
+            </Pagination.Item>
+            <Pagination.Ellipsis
+              onClick={() => {
+                onPageChange(currentPage - siblingsCount - 1);
+              }}
+            />
+          </>
+        )}
+        {previousPages.length > 0 &&
+          previousPages.map((page) => {
+            return (
+              <Pagination.Item
+                key={page}
+                onClick={() => {
+                  onPageChange(page);
+                }}
+              >
+                {page}
+              </Pagination.Item>
+            );
+          })}
+
+        <Pagination.Item active>{currentPage}</Pagination.Item>
+
+        {nextPages.length > 0 &&
+          nextPages.map((page) => {
+            return (
+              <Pagination.Item
+                key={page}
+                onClick={() => {
+                  onPageChange(page);
+                }}
+              >
+                {page}
+              </Pagination.Item>
+            );
+          })}
+
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            {currentPage + siblingsCount + 1 < lastPage && (
+              <>
+                <Pagination.Ellipsis
+                  onClick={() => {
+                    onPageChange(currentPage + siblingsCount + 1);
+                  }}
+                />
+              </>
+            )}
+            <Pagination.Item
+              onClick={() => {
+                onPageChange(lastPage);
+              }}
+            >
+              {lastPage}
+            </Pagination.Item>
+          </>
+        )}
+      </Pagination>
+    </div>
+  );
+}
--- src/data/repositories/product.repository.ts
+++ src/data/repositories/product.repository.ts
@@ -1,34 +1,47 @@
-import { Collection } from '../../domain/models/collection'
-import { Product, ProductCollectionItem } from '../../domain/models/product.model'
-import { RelatedProduct } from '../../domain/models/related-product.model'
-import { ProductRepositoty } from '../../domain/repositories/product.repository'
-import { httpClient } from '../config/http-client'
-import { productCollectionMapper } from '../mappers/product.mapper'
+import { Collection } from "../../domain/models/collection";
+import {
+  Product,
+  ProductCollectionItem,
+} from "../../domain/models/product.model";
+import { RelatedProduct } from "../../domain/models/related-product.model";
+import { ProductRepositoty } from "../../domain/repositories/product.repository";
+import { httpClient } from "../config/http-client";
+import { productCollectionMapper } from "../mappers/product.mapper";
 
 export class IProductDepository implements ProductRepositoty {
-  async getProducts(): Promise<Collection<ProductCollectionItem>> {
-    const response = await httpClient.get('products')
-    return productCollectionMapper(response.data)
+  async getProducts(page: number): Promise<Collection<ProductCollectionItem>> {
+    const response = await httpClient.get("products", {
+      params: {
+        page: String(page),
+      },
+    });
+    return productCollectionMapper(response.data);
   }
 
   getProduct(id: number): Promise<Product> {
-    throw new Error('Method not implemented.')
+    throw new Error("Method not implemented.");
   }
   updateProduct(id: number, attributes: Record<string, any>): Promise<Product> {
-    throw new Error('Method not implemented.')
+    throw new Error("Method not implemented.");
   }
   createProduct(attributes: Record<string, any>): Promise<Product> {
-    throw new Error('Method not implemented.')
+    throw new Error("Method not implemented.");
   }
   deleteProduct(id: number): Promise<void> {
-    throw new Error('Method not implemented.')
+    throw new Error("Method not implemented.");
   }
-  addRelatedProduct(productId: number, relatedProductId: number): Promise<RelatedProduct> {
-    throw new Error('Method not implemented.')
+  addRelatedProduct(
+    productId: number,
+    relatedProductId: number
+  ): Promise<RelatedProduct> {
+    throw new Error("Method not implemented.");
   }
-  removeRelatedProduct(productId: number, relatedProductId: number): Promise<void> {
-    throw new Error('Method not implemented.')
+  removeRelatedProduct(
+    productId: number,
+    relatedProductId: number
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }
 
-export default new IProductDepository()
+export default new IProductDepository();
